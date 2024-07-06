dc38d7d3c3ab493d878308c064b579dfca60e47d
yhdchoi <kbidevops@gmail.com>
2024-07-06 23:43:38 +0900

    User search query updated.

--- src/main/java/com/yhdc/jpa/repository/account/UserDslRepositoryImpl.java
+++ src/main/java/com/yhdc/jpa/repository/account/UserDslRepositoryImpl.java
@@ -1,10 +1,6 @@
 package com.yhdc.jpa.repository.account;
 
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Predicate;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.yhdc.jpa.entity.account.QStack;
 import com.yhdc.jpa.entity.account.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -21,7 +17,8 @@ public class UserDslRepositoryImpl implements UserDslRepository {
 
     @Override
     public List<User> searchUser(String keyword) {
-        List<User> searchedUser = jpaQueryFactory
+
+        return jpaQueryFactory
                 .selectFrom(user)
                 .where(user.username.contains(keyword)
                         .or(user.firstName.contains(keyword))
@@ -29,25 +26,6 @@ public class UserDslRepositoryImpl implements UserDslRepository {
                         .or(user.email.contains(keyword))
                         .or(user.phone.contains(keyword)))
                 .fetch();
-
-        return searchedUser;
-    }
-
-
-    private static Predicate searchStackList(String keyword) {
-        QStack qStack = QStack.stack;
-        BooleanBuilder booleanBuilder = new BooleanBuilder();
-        booleanBuilder.and(qStack.userSet.contains(user)).andAnyOf(qStack.name.contains(keyword).or(qStack.level.contains(keyword)));
-        return booleanBuilder;
-    }
-
-
-    private static BooleanExpression eqKeyword(String keyword) {
-        return user.username.contains(keyword)
-                .or(user.firstName.contains(keyword))
-                .or(user.lastName.contains(keyword))
-                .or(user.email.contains(keyword))
-                .or(user.phone.contains(keyword));
     }
 
 }
